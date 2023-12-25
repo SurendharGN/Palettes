9a1abccf7f37583665ac0f4029687c6bc2eb97ec
Surendhar <studyspace.ie@gmail.com>
2023-12-25 10:22:38 +0530

    Executed copy feature,with changing the model to UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css';
 import { Link } from 'react-router-dom';
+import palette from './assets/palette.png'
 
 function App() {
 
@@ -11,8 +12,11 @@ function App() {
         Palettes: Your Ultimate Color Palette Generator.
       </h1>
       
-      <p id='homepage-desc'>Welcome to Palettes, where creativity meets color ! Unleash the power of vibrant hues and design stunning color palettes effortlessly. <span id="hero-section-keyword">Discover. Create. Inspire. &#128171;</span></p>
+      <p id='homepage-desc'>Unleash the power of vibrant hues and design stunning color palettes effortlessly. Utilize the ColorMind API to dynamically generate aesthetically pleasing color palettes. <span id="hero-section-keyword">Discover. Create. Inspire. &#128171;</span></p>
+      
       <Link to="generatePalette" id="call-to-action">Generate</Link>
+
+      <img src={palette} style={{width:'36em',display:'block',margin:'auto',paddingTop:'60px'}}></img>
       <footer id="footer">Made with &#129505; by surendhar</footer>
     </>
   )
--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,13 +1,36 @@
+// React imports
 import React from 'react';
 import { useState,useEffect } from 'react';
+
+// External library imports
 import rgbHex from 'rgb-hex';
 import { GetColorName } from 'hex-color-to-color-name';
 
+import ColorPicker from './ColorPicker';
+
+// Image imports
+import copy from "/src/assets/copy.svg";
+import drag from "/src/assets/drag.png";
+import lock from "/src/assets/lock.svg";
+import unlock from "/src/assets/unlock.svg";
+
 
 const Palette = () => {
+
+  // Stores the palette returned from the ColorMind API
   const [palette,setPalette]=useState([]);
+
+  // React color picker
+  // const [picker,setPicker]=useState(false);
+  // const handleColorPicker=()=>{
+  //   setPicker(true)
+  // }
+
+  
+  // Stores the RGB values
   let color=[];
 
+// Logic for pushing the color palette
   for(let i=0;i<=4;i++){
     console.log("palette",typeof(palette))
     color.push(palette[i]);
@@ -15,51 +38,121 @@ const Palette = () => {
   }
 
 
+
+  // Logic for calling the API when any key is pressed
   useEffect(()=>{
-    handleFetch()
+    handleFetch();
     window.addEventListener("keyup", handleFetch);
     return () => window.removeEventListener("keyup", handleFetch);
-   
   },[]);
+
+  // Handles color picker
+  // const handleColorPick=()=>{
+  //   return (<HexColorPicker color={colorPicker} onChange={setColorPicker}/>)
+  // }
   
+  // Fuction to fetch the palette
   const handleFetch=()=>{
     fetch('http://colormind.io/api/',{
           method: 'POST',
           body: JSON.stringify({
-            model: 'default',
+            model:"ui",
           }),
         }).
     then((res)=>res.json()).
     then((data)=>setPalette(data.result)).catch((err)=>{console.log(err)});
 
   }
+
+  // HTML returned body
   return (
     <div id='palette-container'>
-    <div id='header'>
-      <h1 id='logo'>Palettes</h1>
-      <p id='palette-desc'>Click any key to generate a palette!</p>
+      <div id='header'>
+        
+        <h1 id='logo' >Palettes</h1>
+        <p id='palette-desc'>Click any key to generate a palette!</p>
+        <div id='copy-palette' onClick={() =>{navigator.clipboard.writeText(JSON.stringify(color))}}>
+          {console.log("copied:",JSON.stringify(color))}
+          
+          <img  
+              id='copy' 
+              src={copy} 
+              alt="copy" 
+              style={{
+                height:'20px',
+                }} />
+          <p id="copy-desc">Copy palette</p>
 
-    </div>
+        </div>
+        
+        
+      </div>
       
       <div id='palette-displayer' >
+
+      {/* Mapping logic */}
       {color.map((key,id)=>{
-        
+        console.log("id:",id)
         return(
-          <div id='shade' style={{backgroundColor:`rgb(${key}`}} key={id}>
-            {key && <p style={{fontWeight:'500',fontSize:'32px',fontFamily:'inter',textTransform:'uppercase'}}>{rgbHex(`${key[0]},${key[1]},${key[2]}`)}</p>}
-            {key && <p style={{fontWeight:'500',fontSize:'16px',fontFamily:'inter',}}>{GetColorName(`${rgbHex(`${key[0]},${key[1]},${key[2]}`)}`)}</p>}
+          <div 
+          // onClick={handleColorPicker}
+          className='shade'
+          style={{backgroundColor:`rgb(${key}`
+        }}   
+          key={id}>
             
+            {/* Converts RGB to hex */}
+            {key && <p 
+            style={{
+            fontWeight:'500',
+            fontSize:'32px',
+            fontFamily:'inter',
+            textTransform:'uppercase'}}>
+              {rgbHex(`${key[0]},${key[1]},${key[2]}`)}
+            </p>}
+              
+            {/* Gets the name of the hex color */}
+            
+            {key && 
+            <p 
+           
+            style={{
+              fontWeight:'500',
+              fontSize:'16px',
+              fontFamily:'inter'}}>
+                {GetColorName(`${rgbHex(`${key[0]},${key[1]},${key[2]}`)}`)}
+            </p>}
+
+
+            {/* Displays the features(Copy,drag,lock) */}
+            <div className="features"  >
+              <img  
+              onClick={() =>{navigator.clipboard.writeText(rgbHex(`${key[0]},${key[1]},${key[2]}`))}}
+              
+              id='copy' 
+              src={copy} 
+              alt="copy" 
+              style={{
+                height:'20px',
+                backgroundColor:`rgb(${key}`}} />
+
+              <img  src={drag} alt="drag" id="drag" 
+              style={{
+                height:'20px',
+                width:'20px',
+                backgroundColor:`rgb(${key}`}} />
+
+              <img src={unlock} id='unlock' alt="copy" 
+              style={{
+                height:'20px',
+                backgroundColor:`rgb(${key}`}} />
+            </div>
+
           </div>
-
         )
-        
       })}
-      </div>
-      </div>
-
-        
-    
-      
+        </div>
+    </div>
   )
 }
 
